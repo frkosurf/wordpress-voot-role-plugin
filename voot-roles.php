90c34d614f2150c142f566acdf0335260310ec6d
François Kooman <fkooman@tuxed.net>
2012-12-06 13:59:20 +0100

    finish licensing

--- voot-roles.php
+++ voot-roles.php
@@ -1,13 +1,31 @@
 <?php
 /*
+    Copyright (C) 2012  François Kooman <fkooman@tuxed.net>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
     Plugin Name: VOOT Roles
     Plugin URI: https://github.com/fkooman/wordpress-voot-role-plugin/
     Description: Sets the role of the user based on VOOT group memberships.
     Author: François Kooman <fkooman@tuxed.net>
-    Version: 0.2
+    Version: 0.3
     Author URI: http://fkooman.wordpress.com/
-    License: GPLv3
- */
+    License: GPLv3 or later
+    License URI: http://www.gnu.org/licenses/gpl-3.0.html
+*/
 
 add_action('wp_login',          'vr_set_fetch_voot_role_meta', 10, 2);
 add_action('auth_cookie_valid', 'vr_set_role',                 10, 2);
